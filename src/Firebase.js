d5e826cb00c54bd21f29c40e01ecc1de9157f6eb
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-09-09 10:07:47 -0300

    Primeira etapa do clone

---
+++ src/Firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+
+const firebaseApp = {
+    apiKey: "AIzaSyCEHEgYW-1uGtRNwkyqUzv4IryCpcVP9oQ",
+    authDomain: "instagram-clone-curso-baff7.firebaseapp.com",
+    projectId: "instagram-clone-curso-baff7",
+    storageBucket: "instagram-clone-curso-baff7.appspot.com",
+    messagingSenderId: "34140672940",
+    appId: "1:34140672940:web:1a47d31165ba4155fc6009",
+    measurementId: "G-J1QC8F4PXB"
+};
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage().ref();
+
+export {db,auth,storage};
+
+
+
+
+
