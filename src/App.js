43a9f512f96cac6aaacd12d89fc0b2545eacba03
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-09-09 12:10:27 -0300

    Header do clone

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import {db} from "./Firebase.js";
 
 
 function App() {
 
+  const [user, setUser] = useState("Rafael");
+
   useEffect(()=>{
-    console.log(db)
+    
   },[])
 
   return (
     <div className="App">
-      
+      <div className='header'>
+          <div className='container'>
+          <div className='headerLogo'>
+            <a href=''><img src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cFe0a4.png'/></a>
+          </div>
+
+          {
+            (user)?
+            <div>Olá Rafael</div>
+            :
+            <div className='headerLoginForm'>
+              <form>
+                <input type="text" placeholder="Login"/>
+                <input type="password" placeholder="Senha"/>
+                <input type="submit" name="acao" value="Entrar"/>
+              </form>  
+            </div>
+          }
+          
+        </div>
+      </div>
     </div>
   );
 }
--- src/Firebase.js
+++ src/Firebase.js
@@ -4,7 +4,7 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 
-const firebaseApp = {
+const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyCEHEgYW-1uGtRNwkyqUzv4IryCpcVP9oQ",
     authDomain: "instagram-clone-curso-baff7.firebaseapp.com",
     projectId: "instagram-clone-curso-baff7",
@@ -12,7 +12,7 @@ const firebaseApp = {
     messagingSenderId: "34140672940",
     appId: "1:34140672940:web:1a47d31165ba4155fc6009",
     measurementId: "G-J1QC8F4PXB"
-};
+});
 
 const db = firebase.firestore();
 const auth = firebase.auth();
